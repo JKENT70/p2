dc15723b839beaa2590d6f5d6fffb3a5170f03ad
John Kent <johnkent@g.harvard.edu>
2014-10-02 19:17:46 -0400

    fix meta errors

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 <title>Project 2</title>
 
 <meta charset="utf-8" />
-<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1">
-	<meta name="description" content="project 2 demonstration">
-<meta name"viewport" content="width=device-width, initial scale=1">
+<meta http-equiv="X-UA-Compatible" content="IE=edge, chrome=1"/>
+	<meta name="description" content="project 2 demonstration"/>
+<meta name="viewport" content="width=device-width, initial scale=1"/>
 
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.3/css/bootstrap.min.css">
