b2b3195a740c594b5f5c6eb61c4fba5ea5d7475a
John Kent <johnkent@g.harvard.edu>
2014-10-02 18:30:27 -0400

    changed page layout

--- content.php
+++ content.php
@@ -5,24 +5,29 @@
     <img class="snakes" src="img/password.JPG" alt="Password Image">
     <br><br>
 	<h2>Generate new password</h2>
-		
-		<form method="post" action="index.php">
+		<div style="width:90%; margin: 0 auto;">	
+			<form method="post" action="index.php">
+				<table width="100%" cellpadding="5" cellspacing="0" border="0"
+					<tr>
+						<td>
+							<label name="">Number of Words</label>
+							<input type="text" id="count" name="count">
 
-			<label name="">Number of Words</label>
-			<input type="text" id="count" name="count"><br>
+							<label name="uppercase">Uppercase first letter</label>
+							<input type="checkbox" name="uppercase" value="y" /><br/>
 
-			<label name="uppercase">Uppercase first letter</label>
-			<input type="checkbox" name="uppercase" value="y" /><br/>
+							<label name="symbol">Use a symbol?</label>
+							<input type="checkbox" name="symbol" value="y" /><br/>
 
-			<label name="symbol">Use a symbol?</label>
-			<input type="checkbox" name="symbol" value="y" /><br/>
+							<label name="number">Include a number?</label>
+							<input type="checkbox" name="number" value="y" /><br />
 
-			<label name="number">Include a number?</label>
-			<input type="checkbox" name="number" value="y" /><br />
-
-			<input type="submit" name="submit" value="submit"/>
-			
-		</form>
+							<input type="submit" name="submit" value="submit"/>
+						</td>
+					</tr>
+				</table>			
+			</form>
+		</div>	
     <br><br>
     <div>Your Password is: <?php echo $pword ?></div>
     
